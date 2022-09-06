a6ae9950a1edfd16457ba18c2343acd30a14bdca
irwinj1 <irwinj121989@gmail.com>
2022-09-06 13:47:43 -0600

    componentes vuejs

---
+++ 02-componentes/src/components/Card.vue
@@ -0,0 +1,32 @@
+<template>
+  <Grid>
+    <img :src="url" :alt="nombre" />
+    <h2>{{ nombre }}</h2>
+
+    <a :href="url" target="_blank"> Descargar </a>
+  </Grid>
+</template>
+<script>
+import Grid from "./Grid.vue";
+export default {
+  props: ["url", "nombre"],
+  components: { Grid },
+};
+</script>
+
+<style scoped>
+img {
+  width: 8rem;
+  height: 8rem;
+  background: red;
+  padding: 0.5rem;
+}
+a {
+  text-decoration: none;
+  color: snow;
+  background-color: #301675;
+  padding: 0.3rem;
+  border-radius: 5px;
+  border: 1px solid #555;
+}
+</style>
---
+++ 02-componentes/src/components/Grid.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped>
+div {
+  display: inline-block;
+  text-align: center;
+  border: 1px solid #555;
+  padding: 1%;
+  margin: 4px;
+  border-radius: 5px;
+}
+</style>
