4afd092e4a22c539339dba751e62592b83526f61
irwinj1 <irwinj121989@gmail.com>
2022-09-07 21:33:26 -0600

    primera fase de formularios

--- 04-formularios/src/App.vue
+++ 04-formularios/src/App.vue
@@ -1,6 +1,66 @@
 
 
 <template>
-  <h1>formularios</h1>
+  <div>
+    <h1>formularios</h1>
+    <img width="100" :src="url" />
+    <hr />
+    <form @submit.prevent="addUser">
+      <div>
+        <label>
+          Nombre:
+          <input v-model="nombre" type="text" placeholder="Nombre" required />
+        </label>
+      </div>
+      <div>
+        <label>
+          Edad:
+          <input
+            type="number"
+            v-model="edad"
+            min="15"
+            max="100"
+            placeholder="edad"
+            required
+          />
+        </label>
+      </div>
+      <div>
+        <label>
+          Correo:
+          <input
+            v-model="correo"
+            type="email"
+            placeholder="email@email.com"
+            required
+          />
+        </label>
+      </div>
+
+      <button type="submit">Enviar</button>
+    </form>
+  </div>
 </template>
 
+<script>
+export default {
+  data: () => ({
+    url: "https://cdn.svgporn.com/logos/vue.svg?response-content-disposition=attachment%3Bfilename%3Dvue.svg",
+    nombre: "",
+    edad: 0,
+    correo: "",
+  }),
+  methods: {
+    addUser() {
+      console.log(this.nombre, this.edad, this.correo);
+    },
+  },
+};
+</script>
+
+<style scoped>
+div {
+  text-align: center;
+  margin: 0px 2rem;
+}
+</style>
