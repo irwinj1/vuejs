1c4f779448d737fe398ba4f8c8d4507aeb514d14
irwinj1 <irwinj121989@gmail.com>
2022-09-13 17:39:39 -0600

    sass en vue

---
+++ sass-http/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <h1>Cotizador de Criptomonedas</h1>
+    <grid>
+      <formulario />
+      <Data />
+    </grid>
+  </div>
+</template>
+
+<script>
+import Data from "./components/Data.vue";
+import Formulario from "./components/Formulario.vue";
+import Grid from "./components/Grid.vue";
+export default { components: { Formulario, Data, Grid } };
+</script>
+
---
+++ sass-http/src/components/Formulario.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <form>
+      <div>
+        <label>Moneda Nacionas: </label>
+        <select>
+          <option value="" disabled selected>Seleccione una Moneda...</option>
+          <option value="">MXN</option>
+          <option value="">USD</option>
+          <option value="">EUR</option>
+        </select>
+      </div>
+      <div>
+        <label>Cripto Moneda: </label>
+        <select>
+          <option value="" disabled selected>
+            Seleccione una Criptomoneda...
+          </option>
+          <option value="">BTC</option>
+          <option value="">ETH</option>
+          <option value="">LTC</option>
+        </select>
+      </div>
+      <div>
+        <button type="submit">Cotizar</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
